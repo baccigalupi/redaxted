06a339241fe1b302884e688c77ebb4965eb274b4
Kane Baccigalupi <baccigalupi@gmail.com>
2021-09-25 10:04:57 -0700

    Adds a mod.js entry point for deno??

---
+++ mod.js
@@ -0,0 +1,3 @@
+export { createAction, createActions } from './src/createActions.js';
+export { composeReducers } from './src/composeReducers.js';
+export { createReducer } from './src/createReducer.js';
